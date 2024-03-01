9ade92f3591d288aa2062647b4f3d3fb3730fa4d
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-02-29 23:23:16 -0800

    Add scaffolding

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Sort data by date. For example, if text input has a column given by ISO 8601 date, this tool can sort the data by the date column.";
+
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            # openssl
+            # pkgconfig
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            rust-analyzer
+          ];
+          shellHook = ''
+            rustc --version
+          '';
+        };
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      }
+    );
+}
