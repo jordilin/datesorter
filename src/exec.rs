17ef77cce9bb09b5a1c2a85a2047974f578704a8
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-03-04 22:10:47 -0800

    Wire things up

---
+++ src/exec.rs
@@ -0,0 +1,14 @@
+use std::io::BufRead;
+
+use crate::sort::{sort_by_date, CliArgs};
+use crate::{input, Result};
+
+pub fn parse_and_sort<B: BufRead>(cli_args: CliArgs, reader: B) -> Result<Vec<String>> {
+    let data = input::parse(reader, &cli_args.delimiter, cli_args.date_pos)?;
+    let sorted_data = sort_by_date(data, &cli_args.sort_mode);
+    let sorted_data = sorted_data
+        .iter()
+        .map(|line| line.fields.join(&cli_args.delimiter))
+        .collect();
+    Ok(sorted_data)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod cli;
+pub mod exec;
 pub mod input;
 pub mod sort;
 
